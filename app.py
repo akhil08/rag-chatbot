24c46d81fcc7db58b2a1332e111f6a1617b08148
akhil08 <akhilcuber@gmail.com>
2026-05-29 14:00:06 +0530

    Update app.py with url rag feature

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import requests
+from bs4 import BeautifulSoup
+# import google.generativeai as genai
+# from PIL import Image
 
 from pypdf import PdfReader
 from langchain_text_splitters import CharacterTextSplitter
@@ -19,6 +23,8 @@ load_dotenv()
 
 # Get API key
 api_key = os.getenv("GROQ_API_KEY")
+# Gemini API setup
+# genai.configure(api_key="")
 
 # Create Groq client
 client = OpenAI(
@@ -28,6 +34,67 @@ client = OpenAI(
 
 # Streamlit title
 st.title("PDF RAG Chatbot")
+
+website_url = st.text_input(
+    "Enter website URL:"
+)
+website_text = ""
+
+if website_url:
+
+    try:
+
+        # Download webpage
+        response = requests.get(website_url)
+
+        # Parse HTML
+        soup = BeautifulSoup(
+            response.text,
+            "html.parser"
+        )
+
+        # Extract webpage text
+        website_text = soup.get_text()
+
+        st.success("Website loaded successfully!")
+
+    except:
+        st.error("Failed to load website.")
+
+# Upload image
+# uploaded_image = st.file_uploader(
+#     "Upload an image",
+#     type=["png", "jpg", "jpeg"]
+# )
+# if uploaded_image:
+
+#     st.image(
+#         uploaded_image,
+#         caption="Uploaded Image",
+#         use_container_width=True
+#     )
+
+#     image_question = st.text_input(
+#         "Ask something about the image:"
+#     )
+
+#     if image_question:
+
+        # Open image
+        # image = Image.open(uploaded_image)
+
+        # # Load Gemini vision model
+        # model = genai.GenerativeModel(
+        #     "gemini-2.0-flash"
+        # )
+
+        # Generate response
+        # response = model.generate_content(
+        #     [image_question, image]
+        # )
+
+        # st.write("### Image Answer:")
+        # st.write(response.text)
 
 # Store chat history
 if "messages" not in st.session_state:
@@ -48,15 +115,17 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 # Run only if PDF uploaded
-if uploaded_file:
-
-    # Read PDF
-    pdf_reader = PdfReader(uploaded_file)
+if uploaded_file or website_url:
 
     text = ""
 
-    for page in pdf_reader.pages:
-        text += page.extract_text()
+    # Read PDF only if uploaded
+    if uploaded_file:
+
+        pdf_reader = PdfReader(uploaded_file)
+
+        for page in pdf_reader.pages:
+            text += page.extract_text()
 
     @st.cache_resource
     def create_vectorstore(text):
@@ -67,7 +136,11 @@ if uploaded_file:
             chunk_overlap=100
         )
 
-        chunks = text_splitter.split_text(text)
+        combined_text = text + "\n" + website_text
+
+        chunks = text_splitter.split_text(
+            combined_text
+        )
 
         # Create embeddings
         embeddings = HuggingFaceEmbeddings()
@@ -85,7 +158,7 @@ if uploaded_file:
 
     # User question
     user_question = st.chat_input(
-        "Ask a question about the PDF:"
+        "Ask a question:"
     )
 
     if user_question:
